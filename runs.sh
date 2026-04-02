8f2616351c2baf5d4567acb869421d089e6d19d9
unknown <ramon.gougis@gmail.com>
2026-04-03 00:26:52 +0400

    varying LR for C>512

--- runs.sh
+++ runs.sh
@@ -1,8 +1,13 @@
 for C in 2048 1024 512 256 128 64; do
+    if [ $C -ge 2048 ]; then LR=0.005
+    elif [ $C -ge 1024 ]; then LR=0.01
+    else LR=0.02
+    fi
     python -c "
 import json
 cfg = json.load(open('config.json'))
 cfg['C'] = $C
+cfg['lr'] = $LR
 json.dump(cfg, open('config.json', 'w'), indent=4)
 "
     python train.py
